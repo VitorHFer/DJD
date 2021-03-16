19fb56fc00e78c673b9a0ec51a4402a920b65fba
VitorHFer <72749293+VitorHFer@users.noreply.github.com>
2021-03-16 13:02:43 +0000

    Paralelepipedo
    Crie uma classe Paralelepípedo que permita descrever este objecto através das suas 3 dimensões (altura,largura,comprimento - valores inteiros). Deverá conter os seguintes construtores:
    
    Construtor sem argumentos (colocando os valores das variáveis a 0).
    Construtor apenas com um argumento onde todas as variáveis são colocadas no mesmo valor.
    Construtor onde as 3 variáveis são passadas como argumento. 
    Propriedades para as variáveis. Em qualquer caso, se um dos valores for negativo, deverá ser colocado a zero. 
    Método para o volume do paralelipipedo ( v= a.l.c), 
    Método para o cálculo área total do objeto (área = 2.a.l + 2.a.c + 2.l.c),
    Método override ToString() (p. Ex. “a=4;l=3;c=2”). 
    Deverá ter um método público IsACube() que retornará um valor booleano indicando se o objeto representa um cubo (todos as variáveis têm o mesmo valor).
    Método público Is3D() que retorne true se todas as variáveis forem diferentes de zero (não nulas) e false em caso contrário. 
    
    Crie um programa para testar a classe, todos os métodos e construtores a partir de valores aleatórios.

---
+++ Paralelepipedo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParalelepipedoFinal
+{
+    class Paralelepipedo
+    {
+		public int comprimento;
+		public int largura;
+		public int altura;
+		public int Volume;
+		public int Area;
+
+
+		public Paralelepipedo()
+		{
+			this.comprimento = 3;
+			this.largura = 3;
+			this.altura = 3;
+
+			Console.WriteLine("Comprimento: " + comprimento.ToString());
+			Console.WriteLine("Largura: " + largura.ToString());
+			Console.WriteLine("Altura: " + altura.ToString());
+		}
+
+		public Paralelepipedo(int tam)
+		{
+			this.comprimento = tam;
+			this.largura = tam;
+			this.altura = tam;
+
+			if (tam < 0) tam = 0;
+
+			Console.WriteLine("Comprimento: " + comprimento.ToString());
+			Console.WriteLine("Largura: " + largura.ToString());
+			Console.WriteLine("Altura: " + altura.ToString());
+
+		}
+
+		public Paralelepipedo(int comp, int larg, int alt)
+		{
+			if (comp < 0) comp = 0;
+			if (larg < 0) larg = 0;
+			if (alt < 0) alt = 0;
+
+			this.comprimento = comp;
+			this.largura = larg;
+			this.altura = alt;
+
+
+			Console.WriteLine("Comprimento: " + comp.ToString());
+			Console.WriteLine("Largura: " + larg.ToString());
+			Console.WriteLine("Altura: " + alt.ToString());
+
+		}
+
+		public void volume()
+		{
+			this.Volume = this.comprimento * this.largura * this.altura;
+			Console.WriteLine("Volume: " + this.Volume);
+		}
+
+		public void areaTotal()
+		{
+			this.Area = 2 * altura * largura + 2 * altura * comprimento + 2 * largura * comprimento;
+			Console.WriteLine("Área: " + this.Area);
+		}
+
+		public void IsCube()
+		{
+			if (comprimento == largura && largura == altura && altura  == comprimento)
+			{
+				Console.WriteLine("É um cubo");
+			}
+			else
+			{
+				Console.WriteLine("Não é cubo");
+			}
+		}
+
+		public void Is3D()
+		{
+			if (comprimento == 0 || largura == 0 || altura == 0) Console.WriteLine("É 2D");
+			else Console.WriteLine("É 3D");
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ParalelepipedoFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int a1 = r.Next(0, 10);
+            int b1 = r.Next(0, 10);
+            int c1 = r.Next(0, 10);
+            Paralelepipedo a = new Paralelepipedo();
+            a.volume();
+            a.areaTotal();
+            a.IsCube();
+            a.Is3D();
+
+            Paralelepipedo b = new Paralelepipedo(10);
+            b.volume();
+            b.areaTotal();
+            b.IsCube();
+            b.Is3D();
+
+            Paralelepipedo c = new Paralelepipedo(a1, b1, c1);
+            c.volume();
+            c.areaTotal();
+            c.IsCube();
+            c.Is3D();
+        }
+    }
+}
